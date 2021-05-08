ecb01c9f4423ac3f5387d9bab7456e23fdf8af77
Semen Sokolov <simionsimion@yandex.ru>
2021-05-08 13:24:49 +0300

    Initial commit

---
+++ desktop/src/itis/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package itis.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import itis.game.FlappyBird;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyBird(), config);
+	}
+}
