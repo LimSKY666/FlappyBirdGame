85f35aef4a695406874b4baa0cfd89ae887d821e
Semen Sokolov <simionsimion@yandex.ru>
2021-05-10 14:02:22 +0300

    Some game screen update and logic for bird

--- core/src/itis/game/states/PlayState.java
+++ core/src/itis/game/states/PlayState.java
@@ -1,5 +1,6 @@
 package itis.game.states;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -8,20 +9,25 @@ import itis.game.sprites.Bird;
 
 public class PlayState extends State {
     private Bird bird;
+    private Texture bg;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50,300);
         camera.setToOrtho(false, FlappyBird.WIDTH/2, FlappyBird.HEIGHT/2);
+        bg = new Texture("bg.png");
     }
 
     @Override
     protected void handleInput() {
+        if (Gdx.input.justTouched())
+            bird.jump();
 
     }
 
     @Override
     public void update(float dt) {
+        handleInput();
         bird.update(dt);
     }
 
@@ -29,6 +35,7 @@ public class PlayState extends State {
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(camera.combined);
         sb.begin();
+        sb.draw(bg, camera.position.x - (camera.viewportWidth / 2) , 0);
         sb.draw(bird.getBird(),bird.getPosition().x,bird.getPosition().y);
         sb.end();
     }
